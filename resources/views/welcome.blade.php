7052b7d58d0a57c33055907e80c1db690796a975
ravi1323 <ravi132369@gmail.com>
2022-01-24 09:57:43 +0530

    change metafield api endpoint

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,34 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>This is you : {{ $shopDomain ?? Auth::user()->name }}</p>
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>vendor</th>
+                <th>created</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>vendor</th>
+                <th>created</th>
+            </tr>
+        </tfoot>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product['id'] }}</td>    
+                    <td>{{ $product['title'] }}</td>    
+                    <td>{{ $product['vendor'] }}</td>    
+                    <td>{{ $product['created_at'] }}</td>    
+                </tr>   
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 @section('scripts')
